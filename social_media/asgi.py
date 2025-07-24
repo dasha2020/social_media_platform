a11b60a289992ac03b9455c9d11a960a1da6b059
dasha2020 <darenddhaker128675@gmail.com>
2025-07-24 12:28:56 +0300

    update asgi.py and settings.py

--- social_media/asgi.py
+++ social_media/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import social_media_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            social_media_app.routing.websocket_urlpatterns
+        )
+    ),
+})
--- social_media/settings.py
+++ social_media/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_media_app',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'social_media.wsgi.application'
+ASGI_APPLICATION = "social_media.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 
 # Database
