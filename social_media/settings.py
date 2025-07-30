37aa7e243fc1ddf288b555dd05ed627c061da5f4
dasha2020 <darenddhaker128675@gmail.com>
2025-07-30 12:40:21 +0300

    add profile model

--- social_media/settings.py
+++ social_media/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- social_media_app/apps.py
+++ social_media_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SocialMediaAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social_media_app'
+
+    def ready(self):
+        import social_media_app.signals
