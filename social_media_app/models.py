551ca171b4914cb233688bdebb4bd4eedcf1464d
dasha2020 <darenddhaker128675@gmail.com>
2025-08-06 18:02:22 +0300

    add models: comment, like, post, follower

--- social_media_app/models.py
+++ social_media_app/models.py
@@ -8,3 +8,23 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True)
     admin_in_group = models.BooleanField(default=False)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
